85ce55a4fa3d2db759343ddd2a6fa0ae22efa83d
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2025-05-07 18:53:46 -0400

    fix: change middleware location

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export default function middleware(request: NextRequest) {
-  if (request.url === "https://www.coreum.fun" || request.url === "https://coreum.fun") {
-    return NextResponse.redirect(new URL("/workshop", request.url));
-  }
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/workshop", request.url));
 }
 
 // See "Matching Paths" below to learn more
