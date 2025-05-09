36c75f98a431bb8b7db7935d20c0dcc0275b3984
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2025-05-09 08:41:23 -0400

    fix: remove redirect in localhost

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  //disable local development
+  if (process.env.NODE_ENV === "development") {
+    return NextResponse.next();
+  }
   return NextResponse.redirect(new URL("/workshop", request.url));
 }
 
