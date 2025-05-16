414f5cc59fef658d5f5cf1b61f82f3d6a6b5df68
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2025-05-15 22:09:08 -0400

    feat: add price slice, store and features

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,15 @@ import { generalReducer } from '@/features/general/index';
 import { balancesReducer } from '@/features/balances';
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import ticketsReducer from '@/features/tickets/ticketsSlice';
+import priceReducer from './slices/priceSlice';
+
 export const store = configureStore({
   reducer: {
     general: generalReducer,
     dex: dexReducer,
     balances: balancesReducer,
     tickets: ticketsReducer,
+    price: priceReducer,
   },
 });
 
